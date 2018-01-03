4bc29b314dd686ac1f47cbaec54236eafc785eea
rafaelgianine <rafael.gianine@gmail.com>
2018-01-03 17:48:45 -0200

    Add files via upload

---
+++ ReadXMLToClass/ReadXMLToClass/ExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Management.Automation;
+
+namespace ReadXMLToClass
+{
+    public class ExampleClass
+    {
+        [Alias("Nome")]
+        public string FirstName { get; set; }
+        [Alias("Sobrenome")]
+        public string LastName { get; set; }
+        [Alias("Idade")]
+        public int Age { get; set; }
+    }
+}
